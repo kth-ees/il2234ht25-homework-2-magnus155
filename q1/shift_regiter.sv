6a78fe2760cefa5ba4236e1c9ee786f01763ec5e
magnus155 <mabernt@ug.kth.se>
2025-09-26 16:47:59 +0200

    Update shift_regiter.sv

--- q1/shift_regiter.sv
+++ q1/shift_regiter.sv
@@ -8,6 +8,27 @@ module shift_register #(parameter N=4)
                        output logic [N-1:0] parallel_out,
                        output logic serial_out);
 
-//complete here
+
+    logic [N-1:0] middle;
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            middle       <= '0;
+        end else if (load_enable) begin
+            if (serial_parallel) begin
+                middle <= parallel_in;           
+            end else begin
+                middle <= {middle[N-2:0], serial_in}; 
+            end
+
+
+        end
+
+    
+           assign parallel_out = middle;
+           assign serial_out   = middle[N-1];
+	end
+
 
 endmodule
+
